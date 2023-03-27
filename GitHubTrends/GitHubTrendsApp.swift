0c031dab0f80fc121be181800615ecca1c0811bc
Vadym Pavlov <vadym.pavlov@trafi.com>
2023-03-27 13:39:43 +0300

    Initial commit. Fresh project

---
+++ GitHubTrends/GitHubTrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTrendsApp.swift
+//  GitHubTrends
+//
+//  Created by Vadym Pavlov on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
